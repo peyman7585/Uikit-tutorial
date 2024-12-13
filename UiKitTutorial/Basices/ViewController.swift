f7fe6d2f116dfafa8db043b3f76d4d2062b4ca0b
peyman7585 <abbaszadeh.8575@gmail.com>
2024-12-13 23:33:54 +0330

    add button and label

--- UiKitTutorial/Basices/ViewController.swift
+++ UiKitTutorial/Basices/ViewController.swift
@@ -8,12 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var greetingLabel: UILabel!
+    
+    @IBOutlet weak var primaryButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
 
-
+    func greetUser(_ name: String) {
+        greetingLabel.text = "Hello \(name)"
+    }
+    
+    @IBAction func primaryButtonTapped(_ sender: Any) {
+        
+        greetUser("dance with sky")
+        
+        view.backgroundColor = .white
+    }
+    
 }
 
